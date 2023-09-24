c380a200487fe778309e4369d38e9981adb0a225
ErwinSlijk <erwinslijkerman@yahoo.com>
2023-09-24 17:18:14 +0200

    algemene_functies toegevoegd

---
+++ algemene_functies.py
@@ -0,0 +1,23 @@
+def mijn_functie_1(a, b, c, d): 
+    return a * 2, b * 4, c * 10, d * 12
+
+print(mijn_functie_1(2, 4, 10, 12))
+
+print()
+
+def mijn_functie_2(a, b, c, d):
+    lijst_1 = [a + 2.7, a - 3.3, a + 23.7, a - 8.3]
+    string_list = list(map(int, lijst_1))
+    print(string_list)
+    lijst_2 = [b + 1.8, b - 2.2, b + 11.8, b - 5.2]
+    string_list = list(map(int, lijst_2))
+    print(string_list)
+    lijst_3 = [c + 4.5, c - 5.5, c + 39.5, c - 8.5]
+    string_list = list(map(int, lijst_3))
+    print(string_list)
+    lijst_4 = [d + 19.80, d - 20.20, d + 1899.80, d - 95.15]
+    string_list = list(map(int, lijst_4))
+    print(string_list)
+mijn_functie_2(12.3, 12.2, 10.5, 100.20)
+
+print()
---
+++ reclame.py
@@ -0,0 +1,52 @@
+from algemene_functies import mijn_functie_2
+
+def mijn_functie_1():
+    smaak = "aardbei"
+    prijs = 4
+    korting = 4 - 0.1 * 4
+    print (f"Vandaag in de aanbieding: emmertje ijs (1 liter) in de smaak {smaak}, van {prijs} euro voor {korting:.2f} euro.")
+mijn_functie_1()
+
+print()
+
+def inkomsten_totaal(inkomsten):
+    totaal = inkomsten
+    bedrag = totaal / 100 * 9
+    print (f"Het totaal van alle inkomsten van deze week is {totaal} euro, ")
+    print (f"waarover {bedrag} euro btw betaald dient te worden.")
+inkomsten_totaal(220 + 430 + 125 + 160 + 205 + 90 + 345)
+
+print()
+
+mijn_lijst_2 = [220, 430, 125, 160, 205, 90, 345]
+def laag_en_hoog(mijn_lijst):
+    max_num = max(mijn_lijst_2)
+    min_num = min(mijn_lijst_2)
+    print(f"Op een topdag verdiende we {max_num:.2f} euro")
+    print(f"Op de slechste dag verdiende we {min_num:.2f} euro")
+laag_en_hoog(mijn_lijst_2)
+
+print()
+
+def gemiddelde(mijn_lijst):
+    inkomsten = mijn_lijst
+    bedrag = inkomsten / 7
+    print (f"De gemiddelde inkomsten deze week zijn {bedrag:.2f} euro.")
+gemiddelde(220 + 430 + 125 + 160 + 205 + 90 + 345)
+
+print()
+
+mijn_lijst_3 = [10, 5, 3, 2, 1, 2, 9]
+def meervoudig(invoer_lijst):
+    max_num = max(invoer_lijst)
+    min_num = min(invoer_lijst)
+    print(f"De hoogste waarde is {max_num}")
+    print(f"De laagste waarde is {min_num}")
+meervoudig(mijn_lijst_3)
+
+print()
+
+def combinatie(invoer_lijst_2):
+    korte_lijst = laag_en_hoog(invoer_lijst_2)
+    uitvoer = mijn_functie_2(korte_lijst[0], korte_lijst[1])
+    return uitvoer
